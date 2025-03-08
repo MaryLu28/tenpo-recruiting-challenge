111f746ab2b71b2f4393d2a77fb2d22b5ec71d90
María Lourdes <marylo281292@gmail.com>
2025-03-08 20:37:37 -0300

    Add base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactNode } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import {
+  BaseContainer,
+  BaseContent,
+  MainContainer,
+} from "./shared/common-components";
+
+interface layoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: layoutProps) => {
+  return (
+    <MainContainer>
+      <BaseContainer>
+        <BaseContent>{children}</BaseContent>
+      </BaseContainer>
+    </MainContainer>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<>Login</>} />
+          <Route path="/" element={<>home</>} />
+        </Routes>
+      </Router>
+    </Layout>
   );
 }
 
---
+++ src/shared/common-components.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  min-height: 100vh;
+`;
+
+export const BaseContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+export const BaseContent = styled.div`
+  max-width: 1140px;
+  width: 100%;
+  padding: 0 20px;
+`;
